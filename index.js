63d976c99c369a536e1b3d51596a0c79a71789d1
Anup <anuppoudel60@gmail.comm>
2021-08-14 17:08:02 +1000

    attack function added

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
 let myGridSize = prompt("Enter the size of your grid?");
 let enemyGridSize = prompt("Enter the size of your enemy grid?");
 
+let myShip = 3;
+let enemyShip = 3;
+let enemyLocation = {};
+
+// printGrid(enemyGrid, true);
+// printGrid(myGrid);
+
+const placeCharacter = (y, x, c, grid) => {
+  grid[y][x] = c;
+};
+
 const createGrid = (size) => {
-  let grid = [];
+  let grid = []; // each element inside the array is array itself
   for (let i = 0; i < size; i++) {
     grid[i] = [];
     for (let j = 0; j < size; j++) {
@@ -16,14 +27,60 @@ const createGrid = (size) => {
 let myGrid = createGrid(myGridSize);
 let enemyGrid = createGrid(enemyGridSize);
 
-const createHeader = (size) => {
-  let result = "  ";
-  for (let i = 0; i < size; i++) {
-    result = result + i + "  ";
+const getRandomInt = (max) => {
+  return Math.floor(Math.random * Math.floor(max));
+};
 
-    return result;
+const placeRandomCharacter = (c, grid, max) => {
+  let didPlace = false;
+  while (!didPlace) {
+    let x = getRandomInt(max);
+    let y = getRandomInt(max);
+
+    if (!enemyLocation[`${x}-${y}`]) {
+      placeCharacter(x, y, c, grid);
+      didPlace = true;
+      enemyLocation[`${x}-${y}`] = true;
+    }
   }
 };
+
+// game setup
+for (let i = 1; i < 4; i++) {
+  let x = prompt("Enter the X cordinate for your ship number? " + i);
+  let y = prompt("Enter the Y cordinate for your ship number?" + i);
+  placeCharacter(x, y, "O", myGrid);
+  placeRandomCharacter("O", enemyGrid, enemyGridSize);
+  drawBreak();
+  printGrid(enemyGrid, true);
+  printGrid(myGrid);
+}
+
+// game loop
+while (enemyShip > 0 && myShip > 0) {
+  let x = prompt("Enter X cordinate for your attack");
+  let y = prompt("Enter Y cordinate for your attack");
+
+  if (attackFunction(x, y, enemyGrid)) {
+    enemyShip--;
+  }
+  x = getRandomInt(myGridSize);
+  y = getRandomInt(myGridSize);
+
+  if (enemyShip > 0 && attackFunction(x, y, myGrid)) {
+    myShip--;
+  }
+
+  drawBreak();
+  printGrid(enemyGrid, true);
+  printGrid(myGrid);
+}
+
+// if (myShip < enemyShip) {
+//   console.log("YOU HAVE LOST");
+// } else {
+//   console.log("VICTORY!!!!!");
+// }
 
 const printGrid = (grid, isEnemy = false) => {
   const headers = createHeader(grid.length);
@@ -31,7 +88,7 @@ const printGrid = (grid, isEnemy = false) => {
   for (let i = 0; i < grid.length; i++) {
     let rowStr = i + " ";
     for (let cell of grid[i]) {
-      if (isEnemy && cell == "S") {
+      if (isEnemy && cell == "O") {
         rowStr = rowStr + "- ";
       } else {
         rowStr = rowStr + cell + "  ";
@@ -41,5 +98,26 @@ const printGrid = (grid, isEnemy = false) => {
   }
 };
 
-printGrid(enemyGrid, true);
-printGrid(myGrid);
+const createHeader = (size) => {
+  let result = "  ";
+  for (let i = 0; i < size; i++) {
+    result = result + i + "  ";
+  }
+  return result;
+};
+
+const attackFunction = (x, y, grid) => {
+  if (grid[y][x] == "O") {
+    grid[y][x] == "!!!";
+    return true;
+  } else if (grid[y][x] == "-") {
+    grid[y][x] = "x";
+    return false;
+  } else {
+    return false;
+  }
+};
+
+const drawBreak = () => {
+  console.log("-----------------------------");
+};
